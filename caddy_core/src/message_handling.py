396a32fc16a3a8b9c97548b1e9958c6aecfa5d3d
AndreasThinks <avarotsis@no10.gov.uk>
2024-04-04 18:20:44 +0100

    started moving core caddy functions to separate library

---
+++ caddy_core/src/message_handling.py
@@ -0,0 +1,19 @@
+from .models import CaddyMessageEvent
+
+import os
+import boto3
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.core import patch_all
+
+patch_all()
+
+serverless = boto3.client("lambda")
+
+
+@xray_recorder.capture()
+def handle_message(event: CaddyMessageEvent):
+    serverless.invoke(
+        FunctionName=f'llm-{os.getenv("STAGE")}',
+        InvocationType="Event",
+        Payload=event.model_dump_json(),
+    )
