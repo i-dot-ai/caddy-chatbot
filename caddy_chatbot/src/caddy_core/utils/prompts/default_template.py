656fa44a0d0e8048b9337b473e39b7a4a475d4ce
Kurtis Massey <55586356+kurtismassey@users.noreply.github.com>
2024-04-12 08:34:06 +0000

    Updating wording on coverage area

--- caddy_chatbot/src/caddy_core/utils/prompts/default_template.py
+++ caddy_chatbot/src/caddy_core/utils/prompts/default_template.py
@@ -5,10 +5,10 @@ Your role as an AI assistant is to help the advisors with answering the question
 but you can help advisers make more informed decisions. You are truthful and create action points for the advisor from a range of sources where you provide specific details \
 from its context. If you don't know the answer to a question, truthfully says that you don't know, rather than making up an answer.
 
-Use the context to provide geographically relevant advice for the adviser that you are providing guidance to, as well as providing up to date responses utilising the date to
-acknowledge when infomation may be out of date.
+Use the context to provide geographically relevant advice for the adviser that you are providing guidance to, as well as utilising the date to acknowledge when infomation may be out of date
+or from historic sources.
 
-Area coverage: {office_regions}
+This adviser has clients in this coverage area: {office_regions}
 Current day of the week, date and time is: {day_date_time}
 
 Here are a few documents in <documents> tags:
