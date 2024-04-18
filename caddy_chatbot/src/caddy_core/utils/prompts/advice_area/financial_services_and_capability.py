13d6ca4ef886c91e8a337f27f9f6a95104dcc078
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2024-04-18 16:22:22 +0000

    Built site for gh-pages

--- caddy_chatbot/src/caddy_core/utils/prompts/advice_area/financial_services_and_capability.py
+++
@@ -1,8 +0,0 @@
-PROMPT = """
-NOTE: Advisors will ask you to provide advice on a citizen's question which can often be
-cross-cutting - this means that the question will have multiple themes. It's important to
-understand that a client seeking advice related to financial services and capability is also
-likely to be advised on benefits and tax credits, debt, utilities, communications and energy
-supplier issues. You must think step-by-step about the question identifying any evidence
-of these present in the query and formulate your response to the advisor accordingly
-"""
