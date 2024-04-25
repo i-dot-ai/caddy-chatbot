6ded316b4362c623f5b5a69a990c86aeba67c005
Chetan Chauhan-Sims <86252180+ChetanChauhanSims@users.noreply.github.com>
2024-04-25 15:01:20 +0100

    Update employment.py

--- caddy_chatbot/src/caddy_core/utils/prompts/advice_area/employment.py
+++ caddy_chatbot/src/caddy_core/utils/prompts/advice_area/employment.py
@@ -12,6 +12,14 @@ If your employer turned down your flexible working request on the basis that the
 rules apply to everyone regardless of their circumstances,  you might be able to show this is
 discriminatory because the rule about working evenings and weekends has a bigger impact on you
 because you have childcare or caring responsibilities.
+-----
+Another example is: "I’ve been dismissed for redundancy after 22 years of employment. How much redundancy pay am I entitled to receive from my employer? Am I entitled to any other payments? I am 64 years old on the date of dismissal and my average weekly earning were £650" you could respond with:
+-----
+You are entitled to £19,290 statutory redundancy pay as a minimum amount, your contract of employment may say that you are entitled to more. 
+
+You can check this using the Gov.uk Redundancy Pay calculator and by asking your employer if you are entitled to contractual redundancy pay.
+
+You are also entitled to be paid wages up to the date of dismissal and any accrued but untaken holiday pay. You are entitled to receive 12 weeks notice, which the employer can either make you work or make a payment in lieu of notice - there may be other contractual payments due on dismissal, depending on the terms and conditions of employment.
 ----
 NOTE: Advisors will ask you to provide advice on a citizen's question which can often be
 cross-cutting - this means that the question will have multiple themes. These themes could be
