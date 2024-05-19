0db3ff7139665e5b5fc76eb3bf81de0a5065eb4a
Kurtis Massey <55586356+kurtismassey@users.noreply.github.com>
2024-05-19 08:08:05 +0000

    Initialisation handling

--- caddy_chatbot/src/caddy_core/services/router.py
+++ caddy_chatbot/src/caddy_core/services/router.py
@@ -2,14 +2,23 @@ import boto3
 from semantic_router import Route, RouteLayer
 from semantic_router.encoders import BedrockEncoder
 
-credentials = boto3.Session().get_credentials()
-session_token = credentials.token or ""
-embeddings = BedrockEncoder(
-    access_key_id=credentials.access_key,
-    secret_access_key=credentials.secret_key,
-    session_token=session_token,
-    region="eu-west-3",
-)
+try:
+    credentials = boto3.Session().get_credentials()
+    embeddings = BedrockEncoder(
+        access_key_id=credentials.access_key,
+        secret_access_key=credentials.secret_key,
+        session_token=credentials.token or "",
+        region="eu-west-3",
+    )
+except Exception as error:
+    print(f"Error occured: {error}")
+    credentials = boto3.Session().get_credentials()
+    embeddings = BedrockEncoder(
+        access_key_id=credentials.access_key,
+        secret_access_key=credentials.secret_key,
+        session_token=credentials.token or "",
+        region="eu-west-3",
+    )
 
 benefits_and_tax_credits_route = Route(
     name="benefits_and_tax_credits",
