d4e5f23862a2e0ebe966e74b5e697a2e17ceaca7
AndreasThinks <avarotsis@no10.gov.uk>
2024-07-08 16:53:46 +0100

    changed async handling

--- caddy_chatbot/src/integrations/microsoft_teams/verification.py
+++ caddy_chatbot/src/integrations/microsoft_teams/verification.py
@@ -1,7 +1,10 @@
 from botbuilder.core import (
     BotFrameworkAdapter,
     BotFrameworkAdapterSettings,
+    TurnContext,
 )
+import asyncio
+
 
 import os
 
@@ -12,3 +15,11 @@ APP_PASSWORD = os.getenv("MicrosoftAppPassword", "")
 # Create adapter
 SETTINGS = BotFrameworkAdapterSettings(APP_ID, APP_PASSWORD)
 teams_adapter = BotFrameworkAdapter(SETTINGS)
+
+
+def on_turn(turn_context: TurnContext):
+    if turn_context.activity.type == "message":
+        # Use run_sync to run the asynchronous send_activity method
+        asyncio.run_sync(
+            turn_context.send_activity(f"You said: {turn_context.activity.text}")
+        )
