7b1d90e1d61fd6f6a8b4255abe0d70ecfbcb4fde
Kurtis Massey <55586356+kurtismassey@users.noreply.github.com>
2024-08-17 15:17:48 +0000

    ECS token refresh

--- caddy_chatbot/src/caddy_core/services/router.py
+++ caddy_chatbot/src/caddy_core/services/router.py
@@ -1,17 +1,62 @@
 import os
 import boto3
+from botocore.exceptions import ClientError
+from datetime import datetime, timezone, timedelta
+
 from semantic_router import Route, RouteLayer
 from semantic_router.encoders import BedrockEncoder
 from caddy_core.utils.monitoring import logger
 
-session = boto3.Session()
-credentials = session.get_credentials()
-embeddings = BedrockEncoder(
-    access_key_id=credentials.access_key,
-    secret_access_key=credentials.secret_key,
-    session_token=credentials.token,
-    region="eu-west-3",
-)
+
+class AutoRefreshBedrockEncoder:
+    def __init__(self, region="eu-west-3", score_threshold=0.5):
+        self.region = region
+        self.score_threshold = score_threshold
+        self.encoder = None
+        self.refresh_credentials()
+
+    def refresh_credentials(self):
+        try:
+            sts_client = boto3.client("sts")
+            role_arn = os.environ.get("TASK_ROLE_ARN")
+            response = sts_client.assume_role(
+                RoleArn=role_arn,
+                RoleSessionName="CaddyTaskSession",
+                DurationSeconds=3600,
+            )
+
+            credentials = response["Credentials"]
+            self.encoder = BedrockEncoder(
+                access_key_id=credentials["AccessKeyId"],
+                secret_access_key=credentials["SecretAccessKey"],
+                session_token=credentials["SessionToken"],
+                region=self.region,
+                score_threshold=self.score_threshold,
+            )
+
+            self.expiration = credentials["Expiration"]
+            logger.info(
+                f"Refreshed credentials, new expiry time: {
+                        self.expiration}"
+            )
+        except ClientError as e:
+            logger.error(f"Failed to refresh credentials: {e}")
+            raise
+
+    def __call__(self, *args, **kwargs):
+        if datetime.now(timezone.utc) >= self.expiration - timedelta(minutes=5):
+            logger.info("Credentials expiring soon, refreshing...")
+            self.refresh_credentials()
+
+        return self.encoder(*args, **kwargs)
+
+    def __getattr__(self, name):
+        if name == "score_threshold":
+            return self.score_threshold
+        return getattr(self.encoder, name)
+
+
+embeddings = AutoRefreshBedrockEncoder(region="eu-west-3", score_threshold=0.5)
 
 dynamodb = boto3.resource("dynamodb")
 table = dynamodb.Table(os.getenv("ROUTES_TABLE_NAME"))
@@ -32,5 +77,4 @@ def get_routes_dynamically():
 
 
 routes = get_routes_dynamically()
-
 get_route = RouteLayer(encoder=embeddings, routes=routes)
--- caddy_chatbot/src/caddy_core/utils/prompt.py
+++ caddy_chatbot/src/caddy_core/utils/prompt.py
@@ -17,7 +17,6 @@ def get_prompt(prompt_name):
 
 
 def retrieve_route_specific_augmentation(query):
-    refresh_session_token()
     route = get_route(query).name
     logger.info(f"Route returned: {route}")
 
@@ -29,32 +28,3 @@ def retrieve_route_specific_augmentation(query):
         route_specific_augmentation = get_prompt("FALLBACK_PROMPT")
 
     return route_specific_augmentation, route
-
-
-def refresh_session_token():
-    expiration_timestamp = os.environ.get("AWS_CREDENTIAL_EXPIRATION")
-    if expiration_timestamp:
-        expiration = datetime.fromisoformat(expiration_timestamp)
-        if datetime.now(timezone.utc) < expiration - timedelta(minutes=5):
-            logger.info("Credentials still valid, no refresh required")
-            return
-
-    try:
-        sts_client = boto3.client("sts")
-        temporary_credentials = sts_client.get_session_token(DurationSeconds=3600)
-
-        credentials = temporary_credentials["Credentials"]
-
-        os.environ["AWS_ACCESS_KEY_ID"] = credentials["AccessKeyId"]
-        os.environ["AWS_SECRET_ACCESS_KEY"] = credentials["SecretAccessKey"]
-        os.environ["AWS_SESSION_TOKEN"] = credentials["SessionToken"]
-
-        expiration_iso = credentials["Expiration"].astimezone(timezone.utc).isoformat()
-        os.environ["AWS_CREDENTIAL_EXPIRATION"] = expiration_iso
-
-        logger.info(
-            f"Refreshed credentials, new expiry time: {os.environ['AWS_CREDENTIAL_EXPIRATION']}"
-        )
-    except ClientError as e:
-        logger.error(f"Failed to refresh credentials: {e}")
-        raise
