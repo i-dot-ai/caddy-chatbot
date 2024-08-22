05297e86f9af5618087533a00c5a3c74c75fbb3f
mhgov <max.hollingdale@cabinetoffice.gov.uk>
2024-08-22 14:25:49 +0100

    eval notebooks and topic modelling + cors updates for teams

--- caddy_chatbot/src/integrations/microsoft_teams/responses.py
+++ caddy_chatbot/src/integrations/microsoft_teams/responses.py
@@ -1,5 +1,16 @@
 from fastapi import status
 from fastapi.responses import JSONResponse, Response
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # --- Status Responses --- #
 NO_CONTENT = Response(status_code=status.HTTP_204_NO_CONTENT)
